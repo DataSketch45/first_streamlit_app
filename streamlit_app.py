56dda9bb8a75507d59263a96601042850f99445f
DataSketch45 <106976067+DataSketch45@users.noreply.github.com>
2022-06-07 06:44:33 -0500

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfr
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #Testing
-my_cur.execute("insert into fruit_load_list values ('from streamlit')"
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
